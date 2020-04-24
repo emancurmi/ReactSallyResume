a087093657208ebf61417b0bea0c4ad279d5c2c2
Eman Curmi <emancurmi@gmail.com>
2020-04-24 11:24:22 -0400

    convert project to react

---
+++ script.js
@@ -0,0 +1,36 @@
+﻿const STORAGE = {
+
+    userinfo = {
+        name: 'Sally Student',
+        title: 'Thinkful Student',
+        about_me: "lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. <u>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</u> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of <strong>Lorem Ipsum</strong>.",
+        profileimg: 'https://m.media-amazon.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_.jpg',
+        address: '20 W 34th St, New York, NY 10001',
+        email: 'info@sallystudent.com',
+        phone: 'Tel:(999)999-9999'
+    },
+
+    education = {
+        schoolname: 'Thinkful',
+        coursename: 'Software Engineering Flex Program',
+        startdate: '1/9/2020',
+        enddate: 'present'
+    },
+    skills =['HTML', 'CSS', 'Javascript'],
+    workexperience =[
+        {
+            company: 'IBM',
+            posaition: 'Web Developer',
+            startdate: '2010',
+            enddate: '2012',
+            description: 'lorem ipsum is simply dummy text of the printing and typesetting industry.'
+        },
+        {
+            company: 'Apple',
+            position: 'Fron End Developer',
+            startdate: '2012',
+            enddate: 'pressent',
+            description: 'lorem ipsum is simply dummy text of the printing and typesetting industry.'
+        }
+    ]
+};
